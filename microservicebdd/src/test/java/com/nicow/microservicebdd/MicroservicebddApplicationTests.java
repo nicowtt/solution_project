2d6c61a513477e48ab4d5d787458968bab533395
nicow <nicobod31@gmail.com>
2020-03-13 11:28:57 +0100

    add 3 folders for applications

---
+++ microservicebdd/src/test/java/com/nicow/microservicebdd/MicroservicebddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicow.microservicebdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicebddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
