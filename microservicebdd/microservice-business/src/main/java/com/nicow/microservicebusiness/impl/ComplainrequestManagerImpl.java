563f9d4e7b822d28cfa304daec77ace3680fbb61
nicow <nicobod31@gmail.com>
2020-11-02 10:21:15 +0100

    forget request is now made by backEnd

--- microservicebdd/microservice-business/src/main/java/com/nicow/microservicebusiness/impl/ComplainrequestManagerImpl.java
+++ microservicebdd/microservice-business/src/main/java/com/nicow/microservicebusiness/impl/ComplainrequestManagerImpl.java
@@ -1,18 +1,25 @@
 package com.nicow.microservicebusiness.impl;
 
 import com.nicow.microservicebusiness.contract.ComplainRequestManager;
+import com.nicow.microservicedao.complainDao.ComplainRequestDao;
 import com.nicow.microservicedao.complainDao.ComplainUserDao;
 import com.nicow.microservicemodel.entities.ComplainRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Component
 public class ComplainrequestManagerImpl implements ComplainRequestManager {
 
     @Autowired
     private ComplainUserDao complainUserDao;
+
+    @Autowired
+    private ComplainRequestDao complainRequestDao;
 
     public Boolean checkIfUserCanUpdateRequestPopularity(ComplainRequest requestInput, String pseudoInput) {
         Optional<String> pseudo = requestInput.getUserWhoChangePopularityList().stream()
@@ -23,6 +30,27 @@ public class ComplainrequestManagerImpl implements ComplainRequestManager {
             return false;
         } else {
             return true;
+        }
+    }
+
+    public void forgetOldRequests(int days) {
+        List<ComplainRequest> requestList = complainRequestDao.findAllByForgottenFalse();
+        Calendar today = Calendar.getInstance();
+
+        Calendar lastResponseCalendarAndSevenDays = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.FRANCE);
+
+        for (int i = 0; i < requestList.size(); i++) {
+            try {
+                lastResponseCalendarAndSevenDays.setTime(sdf.parse(requestList.get(i).getLastResponseDate()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            lastResponseCalendarAndSevenDays.add(Calendar.DAY_OF_MONTH, days);
+            if (today.compareTo(lastResponseCalendarAndSevenDays) > 0) {
+                requestList.get(i).setForgetIt(true);
+                complainRequestDao.save(requestList.get(i));
+            }
         }
     }
 
--- microservicebdd/microservice-web/src/main/java/com/nicow/microserviceweb/controllers/ComplainRequestControlleur.java
+++ microservicebdd/microservice-web/src/main/java/com/nicow/microserviceweb/controllers/ComplainRequestControlleur.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -44,6 +45,7 @@ public class ComplainRequestControlleur {
 
     @GetMapping(value = "/getAllRequestsNotForgotten")
     public List<ComplainRequest> getAllRequestsNotForgotten() {
+        complainRequestManager.forgetOldRequests(7);
         return complainRequestDao.findAllByForgottenFalse();
     }
 
